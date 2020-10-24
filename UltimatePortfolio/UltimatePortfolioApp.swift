256e2a4a132cf7ce6967fc783b5bdea7f63c6237
Calvin Goncalves de Aquino <calvin.de.aquino@clio.com>
2020-10-24 15:34:19 -0700

    Initial Commit

---
+++ UltimatePortfolio/UltimatePortfolioApp.swift
@@ -0,0 +1,17 @@
+//
+//  UltimatePortfolioApp.swift
+//  UltimatePortfolio
+//
+//  Created by Calvin De Aquino on 2020-10-24.
+//
+
+import SwiftUI
+
+@main
+struct UltimatePortfolioApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
